1218fa8c2197e7d3c434f56acae9775650ba3386
Dangill <jaimedan@pm.me>
2019-02-19 22:27:00 -0500

    added run.sh

---
+++ bin/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/zsh
+
+ln -s ~/dots/config/vim/vimrc ~/.vimrc
+
+ln -s ~/dots/config/zsh/zshrc ~/.zshrc
+
+ln -s ~/dots/config/profile ~/.profile
+
+ln -s ~/dots/config/zsh/dangill.zsh-theme ~/.oh-my-zsh/themes/dangill.zsh-theme
+
+touch ~/dots/config/zsh/env_vars
+
+cp ~/dots/config/zsh/banner ~/.banner
+chmod +x ~/.banner
+
+source ~/.zshrc
