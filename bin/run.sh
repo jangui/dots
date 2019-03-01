29be295190475a82cf8af85d1dfd3f63148f0242
Dangill <jaimedan@pm.me>
2019-02-28 22:26:41 -0500

    update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -6,7 +6,7 @@ ln -s ~/dots/config/zsh/zshrc ~/.zshrc
 
 ln -s ~/dots/config/profile ~/.profile
 
-ln -s ~/dots/config/zsh/dangill.zsh-theme ~/.oh-my-zsh/themes/dangill.zsh-theme
+ln -s ~/dots/config/zsh/dangill.zsh-theme ~/.oh-my-zsh/themes/jangui.zsh-theme
 
 touch ~/dots/config/zsh/env_vars
 
