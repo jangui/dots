a2456ebe9ebedb137f04f6328c9b7c598b976509
jangui <jaime.dangui@gmail.com>
2022-01-20 23:39:29 -0500

    updated config and setup script

---
+++ config/polybar/launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar janguiBar 2>&1 | tee -a /tmp/polybar.log & disown
